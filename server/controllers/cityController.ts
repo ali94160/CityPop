cd427cf6eac77c3569b072451f2f5d0dcd613abe
Ali94160 <ali.al-mousawi@hotmail.com>
2022-05-23 16:06:20 +0200

    Resloved the bug where you can search for a country in the city-searchfield

--- server/controllers/cityController.ts
+++ server/controllers/cityController.ts
@@ -4,15 +4,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Funtionen hämtar ut en stad med dess id, name och population.
+// Vi kollar även här om det man sökte efter är ett land, isåfall kasta ett felmeddelande.
 export const getCity = async (req: Request, res: Response) => {
   !req.params.cityParam && res.status(404);
   const cityURL = process.env.CITY_URL + req.params.cityParam;
 
   try {
     const { data } = await axios.get(cityURL);
-    if (data.totalResultsCount === 0) {
+    if (
+      data.totalResultsCount === 0 ||
+      data.geonames[0].fclName.includes("country")
+    ) {
       return res.status(401).send("Nothing was found");
     }
+
     const city = {
       id: data.geonames[0].geonameId,
       city: data.geonames[0].toponymName,
